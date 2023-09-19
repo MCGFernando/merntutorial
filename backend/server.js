2340c3944d30166ce2b97e26228fea8ae05fc86d
MCGFernando <marofernando1991@gmail.com>
2023-09-19 13:30:07 +0100

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express')
+const router = require('./routes/workout')
 require('dotenv').config()
 
 
 const app = express()
 
+/* Middleware */
+app.use(express.json())
 app.use('/', (req, res, next)=>{
     console.log(req.path, req.method)
     next()
 })
 
-app.get('/', (req, res)=>{
-    res.json({msg:"Welcome to the app"})
-})
+/* Routes */
 
+app.use('/api/workouts',router)
+
+/* Server */
 app.listen(process.env.PORT, ()=> console.log('Listenning in port ' + process.env.PORT))
